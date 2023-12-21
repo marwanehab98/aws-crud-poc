22f469596b8d76d03c5447f755110115d6cffae8
Marwan Ehab <marwan.el.sheikh14@gmail.com>
2023-12-22 00:08:50 +0200

    extracted redirect page into client component

---
+++ app/components/Redirect.jsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function Redirect() {
+    const router = useRouter();
+    const hash = window.location.hash.slice(1);
+    const splitHash = hash.split('&');
+  
+    useEffect(() => {
+      splitHash.forEach((value) => {
+        const hashObject = value.split('=');
+        if (hashObject[0] === 'expires_in') {
+          let expiration_date = Date.now() / 1000 + parseInt(hashObject[1]);
+          localStorage.setItem('expiration_date', `${expiration_date}`);
+        }
+        localStorage.setItem(hashObject[0], hashObject[1]);
+      });
+  
+      router.push('/');
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [splitHash]);
+  
+    return <></>;
+  }
--- app/redirect-login/page.jsx
+++ app/redirect-login/page.jsx
@@ -1,26 +1,5 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import Redirect from "../components/Redirect";
 
 export default function RedirectUri() {
-  const router = useRouter();
-  const hash = window.location.hash.slice(1);
-  const splitHash = hash.split('&');
-
-  useEffect(() => {
-    splitHash.forEach((value) => {
-      const hashObject = value.split('=');
-      if (hashObject[0] === 'expires_in') {
-        let expiration_date = Date.now() / 1000 + parseInt(hashObject[1]);
-        localStorage.setItem('expiration_date', `${expiration_date}`);
-      }
-      localStorage.setItem(hashObject[0], hashObject[1]);
-    });
-
-    router.push('/');
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [splitHash]);
-
-  return <></>;
+    return <Redirect />;
 }
