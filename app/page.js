5115508764d62749fcef91dfca9914fb5065a1cf
Marwan Ehab <marwan.el.sheikh14@gmail.com>
2023-12-22 00:04:38 +0200

    clean up

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import MainSegment from './components/MainSegment';
 export default function Home() {
   return (
     <div className="space-y-8 bg-transparent">
-      <MainSegment></MainSegment>
+      <MainSegment/>
     </div>
   )
 }
