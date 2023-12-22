126cc38e11d3eb5e8225436620b216656dc732dd
Marwan Ehab <marwan.el.sheikh14@gmail.com>
2023-12-22 02:05:12 +0200

    try

--- app/components/IsAuth.js
+++ app/components/IsAuth.js
@@ -1,11 +1,16 @@
 'use client';
 
 export default function isAuth() {
-  let token = localStorage.getItem('id_token');
-  let expiration_date = localStorage.getItem('expiration_date');
-  if (token && expiration_date) {
-    let dateDiff = parseInt(expiration_date) - Date.now() / 1000;
-    if (dateDiff > 0) return true;
-  }
-  return false;
+    try {
+        let token = localStorage.getItem('id_token');
+        let expiration_date = localStorage.getItem('expiration_date');
+        if (token && expiration_date) {
+            let dateDiff = parseInt(expiration_date) - Date.now() / 1000;
+            if (dateDiff > 0) return true;
+        }
+        return false;
+    } catch (error) {
+        console.log(error)
+    }
+
 }
