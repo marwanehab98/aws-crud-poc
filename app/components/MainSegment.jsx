e5689b4f4d97a84951488756cecb59841f9046bc
Marwan Ehab <marwan.el.sheikh14@gmail.com>
2023-12-22 02:09:17 +0200

    test

--- app/components/MainSegment.jsx
+++ app/components/MainSegment.jsx
@@ -136,7 +136,7 @@ export default function MainSegment() {
                         </div>
                         <div className="h-5"></div>
                         <div className="grid grid-cols-1 gap-24 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
-                            {res.map((value) => {
+                            {/* {res.map((value) => {
                                 return (
                                     <ItemCard
                                         key={value.id}
@@ -148,7 +148,7 @@ export default function MainSegment() {
                                         handleSelect={handleSelectClick}
                                     />
                                 );
-                            })}
+                            })} */}
                         </div>
                     </div>
                 </div>
